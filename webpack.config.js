7ced4919719e4ccfa030d05ed402fabbe930729a
Eran Keydar <ekeydar@gmail.com>
2018-01-01 21:33:15 +0200

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/index.js',
+    watch: true,
+    output: {
+        filename: 'all.js',
+        path: './dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules\//,
+                use: [
+                    //{
+                    //    loader: 'ng-annotate-loader'
+                    //},
+                    {
+                        loader : 'babel-loader',
+                        options: {
+                            presets: ['env']
+                        }
+                    }
+                    ]
+            },
+            {
+                test: /\.html$/,
+                exclude: /node_modules\//,
+                use: [
+                    {
+                        loader: 'html-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.(scss)$/,
+                use: [{
+                    loader: 'style-loader', // inject CSS to page
+                }, {
+                    loader: 'css-loader', // translates CSS into CommonJS modules
+                }, {
+                    loader: 'postcss-loader', // Run post css actions
+                    options: {
+                        plugins: function () { // post css plugins, can be exported to postcss.config.js
+                            return [
+                                require('precss'),
+                                require('autoprefixer')
+                            ];
+                        }
+                    }
+                }, {
+                    loader: 'sass-loader' // compiles SASS to CSS
+                }]
+            },
+        ]
+    },
+    plugins: [
+        // new webpack.ProvidePlugin({
+        //     $: "jquery",
+        //     jQuery: "jquery",
+        //     Popper: 'popper.js',
+        // }),
+       // new webpack.optimize.UglifyJsPlugin()
+    ]
+};
+
+
